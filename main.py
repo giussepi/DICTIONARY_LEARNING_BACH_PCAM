59c0bda17a6b0cf1c5d33ba4ef7ff6abbc7348ae
giussepi <giussepexy@gmail.com>
2020-04-13 20:19:19 +0100

    main.py and requirements.txt updated

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
 # -*- coding: utf-8 -*-
 """  """
 
-import h5py
-import matplotlib.pyplot as plt
-import numpy as np
-
-from dl_algorithms.lc_ksvd.dksvd import DKSVD
+# from dl_algorithms.lc_ksvd.dksvd import DKSVD
 
 
 def main():
     """  """
-    ###########################################################################
-    #                            Reading hdf5 files                           #
-    ###########################################################################
-    # f = h5py.File('datasets/PatchCamelyon (PCam)/camelyonpatch_level_2_split_test_y.h5', 'r')
-    # labels = np.array(f['y']).ravel()
-    # f.close()
-    # with h5py.File('datasets/PatchCamelyon (PCam)/camelyonpatch_level_2_split_test_x.h5', 'r') as f:
-    #     print(f['x'].shape)
-    #     for i in range(0, 500, 20):
-    #         plt.imshow(f['x'][i])
-    #         plt.show()
-
-    ###########################################################################
-    #                                  LCKSVD                                 #
-    ###########################################################################
+    pass
 
 
 if __name__ == '__main__':
